c75fe4c673c9e820cd5f9f22c93217863502cf49
Giorgio Borelli <casaborelli@gmail.com>
2012-03-18 11:21:32 +0100

    fixed subscriber

--- collective/geo/usersmap/subscribers.py
+++ collective/geo/usersmap/subscribers.py
@@ -1,3 +1,4 @@
+from Products.CMFCore.utils import getToolByName
 from plone.app.users.browser.interfaces import IAccountPanelForm
 from zope.component import getUtility
 from interfaces import IUsersCoordinates
@@ -9,12 +10,20 @@ def notify_user_preferences_changed(event):
     context = event.context
     form_data = event.data
     userid = getattr(context, 'userid', None)
-    if not IAccountPanelForm.providedBy(context) or \
-        not userid:
+    props = ['fullname', 'location', 'description']
+
+    if not IAccountPanelForm.providedBy(context):
         return
 
+    for i in props:
+        if i not in form_data.keys():
+            return
+
+    if not userid:
+        mt = getToolByName(context, 'portal_membership')
+        userid = mt.getAuthenticatedMember().id
+
     tool = getUtility(IUsersCoordinates)
-    props = ['fullname', 'location', 'description']
     data = {}
     for el in props:
         data[el] = form_data.get(el, '')
