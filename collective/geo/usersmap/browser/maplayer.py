9433d5d0edec95a3a1879e03c13b102a6e5c316e
Giorgio Borelli <casaborelli@gmail.com>
2012-03-25 19:44:43 +0200

    fixed usersmap kml url

--- collective/geo/usersmap/browser/maplayer.py
+++ collective/geo/usersmap/browser/maplayer.py
@@ -13,12 +13,13 @@ class KMLMapLayer(MapLayer):
         title = self.context.Title().replace("'", "\'")
         if isinstance(title, str):
             title = title.decode('utf-8')
-        context_url = self.context.absolute_url()
-        if not context_url.endswith('/'):
-            context_url += '/'
+        plone_view = self.context.restrictedTraverse('plone_portal_state')
+        plone_url = plone_view.portal_url()
+        if not plone_url.endswith('/'):
+            plone_url += '/'
         template = self.context.restrictedTraverse('%s-layer' % self.name)()
         return template % (title,
-                           context_url)
+                           plone_url)
 
 
 class KMLMapLayers(MapLayers):
